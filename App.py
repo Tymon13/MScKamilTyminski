5cf77a3f54785cceccc3df5d3dd9b7f139bffbf9
Kamil Tymiński <238162@student.pwr.edu.pl>
2022-03-01 01:21:26 +0100

    Separate rest of main to another class

---
+++ App.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from tkinter import ttk
+
+from GraphAnimation import GraphAnimation
+from OptionsFrame import OptionsFrame
+
+
+class App:
+    def __init__(self, tk_root: tk.Tk):
+        self.root = tk_root
+        self.set_dark_theme()
+        self.set_graceful_exit_on_window_close()
+
+        graph_frame = ttk.Frame(tk_root)
+        graph_frame.grid(column=1, row=0)
+        self.graph = GraphAnimation(graph_frame)
+
+        options_frame = ttk.Frame(tk_root)
+        options_frame.grid(column=0, row=0)
+        self.options = OptionsFrame(options_frame)
+
+    def set_graceful_exit_on_window_close(self):
+        self.root.protocol('WM_DELETE_WINDOW', lambda: self.root.quit())
+
+    def set_dark_theme(self):
+        self.root.tk.call('source', 'themes/Azure-ttk-theme/azure.tcl')
+        self.root.tk.call('set_theme', 'dark')
--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 import tkinter as tk
-from tkinter import ttk
 
-from GraphAnimation import GraphAnimation
-from OptionsFrame import OptionsFrame
+from App import App
 
 if __name__ == '__main__':
     root = tk.Tk()
-    root.tk.call('source', 'themes/Azure-ttk-theme/azure.tcl')
-    root.tk.call('set_theme', 'dark')
-
-    graph_frame = ttk.Frame(root)
-    graph_frame.grid(column=1, row=0)
-    graph = GraphAnimation(graph_frame)
-
-    options_frame = ttk.Frame(root)
-    options_frame.grid(column=0, row=0)
-    options = OptionsFrame(options_frame)
-
-    root.protocol('WM_DELETE_WINDOW', lambda: root.quit())
+    app = App(root)
     tk.mainloop()
