d8ecd0e1d11259b059fec979074b0cee62677e1f
Kamil Tymiński <238162@student.pwr.edu.pl>
2022-03-06 12:33:20 +0100

    Prepare GraphAnimation for start/stop buttons
    Also added a way to change model parameters without suddenly changing
    graph animation

--- GraphAnimation.py
+++ GraphAnimation.py
@@ -1,3 +1,4 @@
+import copy
 from tkinter import ttk
 
 import matplotlib
@@ -9,11 +10,17 @@ from ModelCalculator import ModelCalculator
 
 
 class GraphAnimation:
-    def __init__(self, tk_root: ttk.Frame, model: ModelCalculator):
+    def __init__(self, tk_root: ttk.Frame, next_model: ModelCalculator):
         self.root = tk_root
-        self.model = model
-        self.model.reset()
+        self.future_model = next_model
+        self.current_model = copy.deepcopy(next_model)
 
+        self._setup_plot()
+
+        self.animator = None
+        self.start()  # TODO: to be removed when start/stop buttons will be there
+
+    def _setup_plot(self):
         matplotlib.use('TkAgg')
 
         self.fig = plt.figure()
@@ -27,9 +34,23 @@ class GraphAnimation:
         canvas = FigureCanvasTkAgg(self.fig, master=self.root)
         canvas.get_tk_widget().grid()
 
-        self.animator = ani.FuncAnimation(self.fig, self.build_chart, frames=self.model.generate(),
+    def start(self):
+        self.current_model = copy.deepcopy(self.future_model)
+        self.current_model.reset()
+        self.animator = ani.FuncAnimation(self.fig, self._build_chart, frames=self.current_model.generate(),
                                           init_func=lambda: None, interval=100, repeat=False)
 
-    def build_chart(self, frame_data):
+    def _build_chart(self, frame_data):
         self.ax.stackplot(frame_data[0], frame_data[1], frame_data[2], frame_data[3],
                           labels=('Susceptible', 'Infected', 'Recovered'), colors=('gray', 'red', 'blue'))
+
+    def stop(self):
+        self.animator = None
+
+    def pause(self):
+        if self.animator:
+            self.animator.pause()
+
+    def resume(self):
+        if self.animator:
+            self.animator.resume()
