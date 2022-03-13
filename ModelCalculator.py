5ebfb0c643c0b9abbb07848817be7c5efcee16de
Kamil Tymiński <238162@student.pwr.edu.pl>
2022-03-13 20:24:08 +0100

    Fix model calculations
    Well, almost. Not sure what to do about recovered, if I should use the
    c*I formula or assume that people recover after X days

--- ModelCalculator.py
+++ ModelCalculator.py
@@ -22,14 +22,23 @@ class ModelCalculator:
         self.rec = np.zeros(self.frames)
 
     def generate(self):
-        self.sus[0] = self.population
         self.inf[0] = 1  # start the infection
+        self.sus[0] = self.population - self.inf[0]
         for i in range(1, self.frames):
-            # TODO: this assumes that (population > recovery_time) and that all arrays are zeroes at the beginning
-            self.sus[i] = self.sus[i-1] - self.R0
-            recovered = self.inf[i - self.recovery_time]
-            self.inf[i] = self.inf[i-1] * self.R0 - recovered
-            self.rec[i] = self.rec[i-1] + recovered
+            # TODO: this assumes that (frames > recovery_time) and that all arrays are zeroes at the beginning
+            new_infected = self.R0 * self.inf[i-1] * self.sus[i-1] / self.population
+            new_infected = min(new_infected, self.sus[i-1])
+            new_recovered = self.inf[i - self.recovery_time] - self.inf[i - self.recovery_time - 1]
+            new_recovered = max(new_recovered, 0)
+
+            self.sus[i] = self.sus[i-1] - new_infected
+            self.inf[i] = self.inf[i-1] + new_infected - new_recovered
+            self.rec[i] = self.rec[i-1] + new_recovered
+
+            # self.sus[i] = self.sus[i-1] - self.R0
+            # recovered = self.inf[i - self.recovery_time]
+            # self.inf[i] = self.inf[i-1] * self.R0 - recovered
+            # self.rec[i] = self.rec[i-1] + recovered
             yield self.x[:i+1], self.sus[:i+1], self.inf[:i+1], self.rec[:i+1]
 
     # def build_chart(self, frame_number: int, *args):
