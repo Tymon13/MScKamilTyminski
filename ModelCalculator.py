771f732f663c1f080d923d7e02164164606fcfa5
Kamil Tymiński <238162@student.pwr.edu.pl>
2022-03-13 20:38:49 +0100

    Clean up ModelCalculator

--- ModelCalculator.py
+++ ModelCalculator.py
@@ -26,28 +26,13 @@ class ModelCalculator:
         self.sus[0] = self.population - self.inf[0]
         for i in range(1, self.frames):
             # TODO: this assumes that (frames > recovery_time) and that all arrays are zeroes at the beginning
-            new_infected = self.R0 * self.inf[i-1] * self.sus[i-1] / self.population
-            new_infected = min(new_infected, self.sus[i-1])
+            new_infected = self.R0 * self.inf[i - 1] * self.sus[i - 1] / self.population
+            new_infected = min(new_infected, self.sus[i - 1])
             new_recovered = self.inf[i - self.recovery_time] - self.inf[i - self.recovery_time - 1]
             new_recovered = max(new_recovered, 0)
 
-            self.sus[i] = self.sus[i-1] - new_infected
-            self.inf[i] = self.inf[i-1] + new_infected - new_recovered
-            self.rec[i] = self.rec[i-1] + new_recovered
+            self.sus[i] = self.sus[i - 1] - new_infected
+            self.inf[i] = self.inf[i - 1] + new_infected - new_recovered
+            self.rec[i] = self.rec[i - 1] + new_recovered
 
-            # self.sus[i] = self.sus[i-1] - self.R0
-            # recovered = self.inf[i - self.recovery_time]
-            # self.inf[i] = self.inf[i-1] * self.R0 - recovered
-            # self.rec[i] = self.rec[i-1] + recovered
-            yield self.x[:i+1], self.rec[:i+1], self.inf[:i+1], self.sus[:i+1]
-
-    # def build_chart(self, frame_number: int, *args):
-    #     self.sus = np.append(self.sus, [100 - frame_number * 2])
-    #     self.inf = np.append(self.inf, [min(10, frame_number * 2)])
-    #     self.rec = np.append(self.rec, [max((frame_number - 5) * 2, 0)])
-    #     p_rec = plt.fill_between(np.arange(0, frame_number + 1), np.zeros(frame_number + 1), self.rec, color='gray')
-    #     p_inf = plt.fill_between(np.arange(0, frame_number + 1), self.rec, self.rec + self.inf, color='red')
-    #     p_sus = plt.fill_between(np.arange(0, frame_number + 1), self.rec + self.inf, self.rec + self.inf + self.sus,
-    #                              color='blue')
-    #     # p[0].set_color('white')
-    #     # return p
+            yield self.x[:i + 1], self.rec[:i + 1], self.inf[:i + 1], self.sus[:i + 1]
