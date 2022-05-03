7c9feba7a60774e4d38947a981661480cb5aba4a
Kamil Tymiński <238162@student.pwr.edu.pl>
2022-05-03 20:14:34 +0200

    Add vaccination loss option to GUI
    This is becoming a spaghat, but I'm too tired to think of better
    architecture

--- VaccinationOptions.py
+++ VaccinationOptions.py
@@ -14,6 +14,10 @@ class VaccinationOptions(ttk.Frame):
         self.vaccination_daily = tk.DoubleVar(value=self.model.vaccination_daily_percentage)
         self.sub_options = None
 
+        self.vaccination_loss_on = tk.BooleanVar(value=False)
+        self.vaccination_loss_delay = tk.IntVar(value=self.model.vaccination_loss_delay)
+        self.vaccination_loss_options = None
+
         self._setup_entry_callbacks()
         self._make_grid()
 
@@ -42,6 +46,11 @@ class VaccinationOptions(ttk.Frame):
                                             justify='right')
         vaccination_daily_entry.grid(column=2, row=2, sticky=tk.E)
 
+        ttk.Label(self.sub_options, text="Vaccination loss").grid(column=1, row=3, sticky=tk.W)
+        self.vaccination_switch = ttk.Checkbutton(self.sub_options, style='Switch.TCheckbutton',
+                                                  variable=self.vaccination_loss_on)
+        self.vaccination_switch.grid(column=2, row=3, sticky=tk.E)
+
         self.model.vaccination_delay = self.vaccination_delay.get()
         self.model.vaccination_daily_percentage = self.vaccination_daily.get()
         self.sub_options.grid(column=1, row=2, columnspan=2, sticky=tk.EW)
@@ -58,9 +67,34 @@ class VaccinationOptions(ttk.Frame):
             "write", lambda *_: setattr(self.model, "vaccination_delay", self.vaccination_delay.get()))
         self.vaccination_daily.trace_add(
             "write", lambda *_: setattr(self.model, "vaccination_daily_percentage", self.vaccination_daily.get()))
+        self.vaccination_loss_on.trace_add("write", self._switch_vaccination_loss_options)
+        self.vaccination_loss_delay.trace_add(
+            "write", lambda *_: setattr(self.model, "vaccination_loss_delay", self.vaccination_daily.get()))
 
     def _switch_sub_options(self, *_):
         if self.vaccination_on.get():
             self._show_options()
+            self._switch_vaccination_loss_options()
         else:
+            self._switch_vaccination_loss_options()
             self._hide_options()
+
+    def _switch_vaccination_loss_options(self, *_):
+        self.model.vaccination_loss_delay = -1
+
+        if not self.vaccination_loss_options and self.vaccination_loss_on.get() and self.sub_options:
+            self.vaccination_loss_options = ttk.Frame(self.sub_options)
+            self.vaccination_loss_options.columnconfigure(1, weight=1)
+            self.vaccination_loss_options.columnconfigure(2, weight=1)
+
+            ttk.Label(self.vaccination_loss_options, text='Days before vaccine immunity is lost').grid(column=1, row=1,
+                                                                                                       sticky=tk.W)
+            vaccination_delay_entry = ttk.Entry(self.vaccination_loss_options, textvariable=self.vaccination_loss_delay,
+                                                width=3, justify='right')
+            vaccination_delay_entry.grid(column=2, row=1, sticky=tk.E)
+            self.vaccination_loss_options.grid(column=1, row=4, columnspan=2, sticky=tk.EW)
+
+            self.model.vaccination_loss_delay = self.vaccination_loss_delay.get()
+        elif self.vaccination_loss_options:
+            self.vaccination_loss_options.destroy()
+            self.vaccination_loss_options = None
